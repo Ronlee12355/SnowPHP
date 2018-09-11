4ffc2a991d8c47eb458703ee4063f2b1715a22cb
Ronlee12355 <Ronlee12355@outlook.com>
2018-09-11 23:15:17 +0800

    adding uploadclass

--- component/upload.class.php
+++ component/upload.class.php
@@ -1,5 +1,35 @@
 <?php
 namespace component;
 class uploadClass{
-    
+    public $allow_file_type=[];
+    public $file_name;
+    public $file_size=8888888;
+    public $ext;
+    public $allow_mime=[];
+    public $destination;
+
+    public function uploadFile(){
+
+    }
+    private function checkFileSize(){
+
+    }
+    private function checkMime(){
+
+    }
+    private function checkHttpUpload(){
+
+    }
+    private function checkExt(){
+
+    }
+    private function checkVaildDest(){
+        
+    }
+    private function checkError(){
+
+    }
+    private function showError(){
+
+    }
 }
