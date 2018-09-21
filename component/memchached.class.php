042335a2acf2fd901763ae4a19a4a77baff5039e
Ronlee12355 <Ronlee12355@outlook.com>
2018-09-22 00:22:36 +0800

    initiate memcached class

--- component/memchached.class.php
+++ component/memchached.class.php
@@ -2,5 +2,22 @@
 namespace component;
 
 class Mem{
-    
+    protected $type="Memcached";
+    protected $m;
+
+    public function S(string $method='get',array $arrWant){
+
+    }
+
+    private function get(){
+
+    }
+
+    private function set(){
+
+    }
+
+    private function delete(){
+        
+    }
 }
